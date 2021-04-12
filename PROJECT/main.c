ae00d43259666cb9efffdb13265c843d0abb9c94
Yarden Agami <yardenagami@gmail.com>
2021-04-12 23:36:48 +0300

    new

--- PROJECT/main.c
+++ PROJECT/main.c
@@ -3,14 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#define A 1
-#define B 2
-#define C 3
-#define D 4
-#define E 5
-#define F 6
-#define G 7
-#define H 8
 #define ALLOCATION_FAILURE -1
 typedef char chessPos[2];
 typedef struct _chessPosCell
@@ -28,41 +20,186 @@ typedef struct __chessPosList
     chessPosCell* head;
     chessPosCell* tail;
 }chessPosList;
-chessPosArray*** validKnightMoves();
-void display(chessPosList* lst);
-void check_allocation(void* res);
-int isPosAppear(chessPosList* lst, chessPos position);
-chessPosCell* AllocateDListNode(int num, chessPosCell* next);
-void insertDataToEndList(chessPosList* res, int num);
-void insertNodeToEndOfList(chessPosList* lst, chessPosCell* Tail);
-void makeEmptyList(chessPosList* res);
+//void insertDataToEndList(chessPosList* res, int num);
 bool isEmptyList(chessPosList* lst);
 void freeList(chessPosList* lst);
-void printList(chessPosList* res);
+chessPosArray*** validKnightMoves();
+void printList(chessPosList* lst);
+void check_allocation(void* res);
+void display(chessPosList* lst);
+void makeEmptyPosList(chessPosList* lst);
+int isPosAppear(chessPosList* lst, char posFirstChar, char posSecondChar);
+chessPosCell* createNewListNode(chessPos position, chessPosCell* nextNode);
+void insertPosCellToEndList(chessPosList* lst, chessPosCell* node);
 void main()
 {
-    //chessPosArray*** valid_moves = validKnightMoves();
-    chessPosList* lst;
-   // makeEmptyList(lst);
-   // display(lst);
-  //  printList(lst);
-}
-//chessPosArray*** validKnightMoves()
-//{
- //   
-//}
-void check_allocation(void* res)
+    chessPosArray*** valid_moves = validKnightMoves();
+    chessPosCell node9 = { { 'E','1' }, NULL };
+    chessPosCell node8 = { {'E', '1'}, &node9 };
+    chessPosCell node7 = { {'E', '1'}, &node8 };
+    chessPosCell node6 = { {'E', '1'}, &node7 };
+    chessPosCell node5 = { {'A', '4'}, &node6 };
+    chessPosCell node4 = { {'H', '5'}, &node5 };
+    chessPosCell node3 = { {'D', '8'}, &node4 };
+    chessPosCell node2 = { {'H', '5'}, &node3 };
+    chessPosCell node1 = { {'A', '4'}, &node2 };
+   
+    chessPosList newList = { &node1, &node9 };
+    //printList(&newList);
+    display(&newList);
+   // freeList(&newList);
+ }
+chessPosArray*** validKnightMoves()
 {
-    if (!res)
+    chessPosArray*** arr = (chessPosArray***)malloc(sizeof(chessPosArray**));
+    //check_allocation(arr);
+    int i, j;
+    int count = -1;
+    for (i = 0; i < 8; i++)
     {
-        fprintf(stderr, "Allocation failure in \nExiting...\n");
-        exit(ALLOCATION_FAILURE);
+        arr[i] = (chessPosArray**)malloc(sizeof(chessPosArray*));
+        for (j = 0; j < 8; j++)
+        {
+            //arr[i][j] = (chessPosArray*)malloc(sizeof(chessPosArray));
+            //check_allocation(arr[i][j]);
+            if ((j + 1) <= 7 && (i - 2) >= 0)/*one right and two up*/
+            {
+                count++;
+                arr[i][j]->positions[count][0] = (char)(i - 2) + 1 + 'A';
+                arr[i][j]->positions[count][1] = (j + 1) + 1 + '0';
+            }
+            if (j + 2 <= 7 && i - 1 >= 0)/*two right and one up*/
+            {
+                count++;
+                arr[i][j]->positions[count][0] = (char)(i - 1) + 1 + 'A';
+                arr[i][j]->positions[count][1] = (j + 2) + 1 + '0';
+            }
+            if (j + 2 <= 7 && i + 1 <= 7)/*two right and one down*/
+            {
+                count++;
+                //*arr[i][j]->positions[count] = (chessPos*)malloc(sizeof(chessPos));
+                arr[i][j]->positions[count][0] = (char)(i + 1) + 1 + 'A';
+                arr[i][j]->positions[count][1] = (j + 2) + 1 + '0';
+            }
+            if (j + 1 <= 7 && i + 2 <= 7)/*one right and two down*/
+            {
+                count++;
+                arr[i][j]->positions[count][0] = (char)(i+2) + 1 + 'A';
+                arr[i][j]->positions[count][1] = (j + 1) + 1 + '0';
+            }
+            if (j - 1 >= 0 && i + 2 <= 7)/*one left and two down*/
+            {
+                count++;
+                arr[i][j]->positions[count][0] = (char)(i + 2) + 1 + 'A';
+                arr[i][j]->positions[count][1] = (j - 1) + 1 + '0';
+            }
+            if (j - 2 >= 0 && i + 1 <= 7)/*two left and one down*/
+            {
+                count++;
+                arr[i][j]->positions[count][0] = (char)(i + 1) + 1 + 'A';
+                arr[i][j]->positions[count][1] = (j - 2) + 1 + '0';
+            }
+            if (j - 2 >= 0 && i - 1 >= 0)/*two left and one up*/
+            {
+                count++;
+                arr[i][j]->positions[count][0] = (char)(i - 1) + 1 + 'A';
+                arr[i][j]->positions[count][1] = (j - 2) + 1 + '0';
+            }
+            if (j - 1 >= 0 && i - 2 >= 0)/*one left and two up*/
+            {
+                count++;
+                arr[i][j]->positions[count][0] = (char)(i - 2) + 1 + 'A';
+                arr[i][j]->positions[count][1] = (j - 1) + 1 + '0';
+            }
+
+            arr[i][j]->size = count;
+            count = 0;
+        }
     }
+    return arr;
 }
-
-/*void display(chessPosList* lst)
+void display(chessPosList* lst)
 {
-
+    int lstSize = 0;
+    chessPosList tmpList;
+    makeEmptyPosList(&tmpList);
+    chessPosCell* node = lst->head, * insertNode;
+    chessPosCell* tmpNode = tmpNode = node->next;
+    while (node != NULL)
+    {
+        while (tmpNode != NULL && node->position[0] == tmpNode->position[0] && node->position[1] == tmpNode->position[1]) // Cases of a row of the same position
+        {
+            node->next = tmpNode->next;
+            tmpNode->next = NULL;
+           //  free(tmpNode);/* this will not work if there hasn't been an allocation for this memory before*/
+            tmpNode = node->next;
+        }
+        if (tmpNode != NULL)
+        {
+            while (isPosAppear(&tmpList, tmpNode->position[0], tmpNode->position[1])) // Checks if the position already appears in the template (tmpList)
+            {
+                node->next = tmpNode->next;
+                tmpNode->next = NULL;
+               //  free(tmpNode);/* this will not work if there hasn't been an allocation for this memory before*/
+                if (node->next != NULL)
+                {
+                    tmpNode = node->next;
+                }
+            }
+        }
+        if (isPosAppear(&tmpList, node->position[0], node->position[1])) // Checks if the position already appears in the template (tmpList)
+        {
+            node->next = tmpNode->next; /*useless warning*/
+            tmpNode->next = NULL;
+            // free(tmpNode);/* this will not work if there hasn't been an allocation for this memory before*/
+        }
+        insertNode = createNewListNode(node->position, NULL);
+        insertPosCellToEndList(&tmpList, insertNode);
+        node = node->next;
+        if (node != NULL)
+        {
+            tmpNode = node->next;
+        }
+        lstSize++;
+    }
+    *lst = tmpList;
+    printList(lst);
+}
+void printList(chessPosList* lst)
+{
+    chessPosCell* node = lst->head;
+    int j;
+    int i;
+    int count = 1;
+    char sign;
+    char chessarray[8][8][1];
+    while (node)
+    {
+        chessarray[(int)node->position[0] - 65][(int)node->position[1] - 49][0] = count + '0';
+        printf("%c%c ", node->position[0], node->position[1]);
+        count++;
+        node = node->next;
+        
+    }
+    puts("\n");
+    printf("    1 2 3 4 5 6 7 8  \n");  
+    for (i = 0; i < 8; i++)
+    {
+        printf("%c->", i + 'A');
+        for (j = 0;j < 9; j++)
+        {
+            if (chessarray[i][j][0] > 1 && chessarray[i][j][0] <= 64)
+                printf("|%c", chessarray[i][j][0]);
+            else
+                printf("| ");
+        }
+        puts("\n");
+    }
+    puts("--------------------\n");
+}
+void makeEmptyPosList(chessPosList* lst)
+{
+    lst->head = lst->tail = NULL;
 }
 bool isEmptyList(chessPosList* lst)
 {
@@ -71,7 +208,50 @@ bool isEmptyList(chessPosList* lst)
     else
         return false;
 }
-void printList(chessPosList* res)
+int isPosAppear(chessPosList* lst, char posFirstChar, char posSecondChar)
+{
+    chessPosCell* node = lst->head;
+    while (node != NULL)
+    {
+        char lstFirstChar = node->position[0], lstSecondChar = node->position[1];
+        if (lstFirstChar == posFirstChar && lstSecondChar == posSecondChar)
+        {
+            return 1;
+        }
+        node = node->next;
+    }
+    return 0;
+}
+chessPosCell* createNewListNode(chessPos position, chessPosCell* nextNode)
+{
+    chessPosCell* node = (chessPosCell*)malloc(sizeof(chessPosCell));
+    check_allocation(node);
+    node->position[0] = position[0];
+    node->position[1] = position[1];
+    node->next = nextNode;
+
+    return node;
+}
+void insertPosCellToEndList(chessPosList* lst, chessPosCell* node)
+{
+    if (isEmptyList(lst))
+    {
+        lst->head = lst->tail = node;
+    }
+    else
+    {
+        lst->tail->next = node;
+        lst->tail = node;
+    }
+    node->next = NULL;
+}
+/*void insertDataToEndList(chessPosList* res, int num)
+{
+    chessPosCell* node = AllocateDListNode(num, NULL);
+    check_allocation(node);
+    insertNodeToEndOfList(res, node);
+}*/
+/*void printList(chessPosList* res)
 {
     chessPosCell* current = res->head;
     int j;
@@ -97,29 +277,11 @@ void printList(chessPosList* res)
         }
         current = current->next;
     }  
-}
-char* strn(char c,int n)
+}*/
+/*char* strn(char c,int n)
 {
     return  
-}
-void makeEmptyList(chessPosList* res)
-{
-    res->head = res->tail = NULL;
-}
-int isPosAppear(chessPosList* lst, chessPos position)
-{
-	chessPosCell* node = lst->head;
-	while (node != NULL)
-	{
-		if (node->position == position)
-		{
-			return 1;
-		}
-		node = node->next;
-	}
-	return 0;
-}
-
+}*/
 void freeList(chessPosList* lst)
 {
      chessPosCell* curr;
@@ -135,34 +297,13 @@ void freeList(chessPosList* lst)
     lst->head = NULL;
     lst->tail = NULL;
 }
+void check_allocation(void* res)
+{
+    if (!res)
+    {
+        fprintf(stderr, "Allocation failure in \nExiting...\n");
+        exit(ALLOCATION_FAILURE);
+    }
+}
 
-void insertDataToEndList(chessPosList* res, int num)
-{
-    chessPosCell* node = AllocateDListNode(num, NULL);
-    check_allocation(node);
-    insertNodeToEndOfList(res, node);
-}
-chessPosCell* AllocateDListNode(int num, chessPosCell* next)/*allocation of new node*/
-/*{
-    chessPosCell* res;
-    res = (chessPosCell*)malloc(sizeof(chessPosCell));
-    check_allocation(res);
-    *(res->position)= (int*)malloc(sizeof(int));/*allocation of data*/
-  /*  check_allocation(res);
-    *(res->position)= num;
-    res->next = next;
-    return res;
-}
-void insertNodeToEndOfList(chessPosList* lst, chessPosCell* Tail)
-{
-    if (isEmptyList(lst))
-    {
-        lst->head = Tail;
-        lst->tail = Tail;
-    }
-    else
-    {
-        lst->tail->next = Tail;
-        lst->tail = Tail;
-    }
-}*/
+
